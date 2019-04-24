61c3d2a556529c27ac927ff699b018f0080a72e0
Kimmo Hintikka <kimmo.hintikka@amplitude.com>
2019-04-24 22:12:19 +0200

    ready app with no instrumentation

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { LinearGradient } from 'expo';
+import { primaryGradientArray } from './utils/Colors';
+export default class Main extends React.Component {
+  render() {
+    return (
+      <LinearGradient colors={primaryGradientArray} style={styles.container}>
+        <StatusBar barStyle="light-content" />;
+        <Text>Open up App.js to start working on your app!</Text>
+      </LinearGradient>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
